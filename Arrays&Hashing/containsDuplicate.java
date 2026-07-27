2efbff67f03ebab48351c9f5677e126b3dd8ae83
Harsh Sharma <krish9515274@gmail.com>
2026-07-27 16:49:05 +0000

    leet 217

---
+++ Arrays&Hashing/containsDuplicate.java
@@ -0,0 +1,13 @@
+//leet 217
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for(int i:nums){
+            if(seen.contains(i)){
+                return true;
+            }
+            seen.add(i);
+        }
+        return false;
+    }
+}
