4d53e084b705c1ac73d562b0228101448fadbf41
Harsh Sharma <krish9515274@gmail.com>
2026-07-29 09:39:40 +0000

    leet 42

---
+++ TwoPointers/TrappingRainWater.java
@@ -0,0 +1,25 @@
+// leet 42
+//pattern - opposite left and right pointers
+public class Solution{
+    public int trap(int[] height){
+        if(height==null || height.length==0) return 0;
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = height[left];
+        int rightMax = height[rightMax];
+        int res = 0;
+        while(left<right){
+            if(leftMax<rightMax){
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                res+=leftMax-height[left];
+            }
+            else{
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                res+=rightMax-height[right];
+            }
+        }
+        return res;
+    }
+}
