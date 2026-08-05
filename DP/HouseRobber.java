25cafa62105eb05ad312086996a4c91065eba252
Harsh Sharma <krish9515274@gmail.com>
2026-08-05 16:32:43 +0000

    leet 198

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -20,4 +20,20 @@ class Solution {
 
         return dp[i]=Math.max(steal, skip);
     }
+
+    //bottom up approach 
+    public int rob2(int[] nums){
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        
+        dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        
+        return dp[n-1];
+    }
 }
