baa029873904ba1f505f4a8f7c12bd94cc884843
Harsh Sharma <krish9515274@gmail.com>
2026-08-06 13:59:50 +0000

    leet 15

--- TwoPointers/3Sum.java
+++ TwoPointers/3Sum.java
@@ -1,25 +1,21 @@
 //leet 15
 //pattern - opposite left and right pointers 
-public class Solution{
-    public List<List<Integer>> threeSum(int[] nums){
-        Arrays.Sort(nums);
-        List<List<Integer> res = new ArrayList<>();
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             if(nums[i]>0) break;
             if(i>0 && nums[i]==nums[i-1]) continue;
 
-            int left = 0;
+            int left = i+1;
             int right = nums.length-1;
             while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum>0){
-                    right--;
-                }
-                else if(sum<0){
-                    left++;
-                }
+                int sum = nums[left]+nums[right]+nums[i];
+                if(sum>0) right--;
+                else if(sum<0) left++;
                 else{
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
                     left++;
                     right--;
                     while(left<right && nums[left]==nums[left-1]) left++;
@@ -27,5 +23,8 @@ public class Solution{
             }
         }
         return res;
+
+        
+
     }
 }
