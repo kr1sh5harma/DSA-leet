fb1b475de1aac1adec6eb0811e355e01b7c40863
Harsh Sharma <krish9515274@gmail.com>
2026-08-26 09:37:26 +0000

    leet 35

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,13 @@
+public class Solution{
+    public int searchInsertPosition(int[] nums, int target){
+        int st = 0;
+        int end = nums.length-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) end = mid-1;
+            else st = mid+1;
+        }
+        return st;
+    }
+}
