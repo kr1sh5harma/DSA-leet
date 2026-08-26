9db13ea4e98a98c24dc55b3f711c3582d717691f
Harsh Sharma <krish9515274@gmail.com>
2026-08-26 10:23:55 +0530

    leet 46

---
+++ Backtracking/Permutations.java
@@ -0,0 +1,25 @@
+// leet 46
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, result, temp, used);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> temp, boolean[] used){
+        if(temp.size()==nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(used[i]) continue;
+            temp.add(nums[i]);
+            used[i]=true;
+            backtrack(nums, result, temp, used);
+            temp.remove(temp.size()-1);
+            used[i] = false;
+        }
+    }
+}
