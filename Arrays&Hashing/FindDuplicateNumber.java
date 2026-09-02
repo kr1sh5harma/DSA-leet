b4f97c9444bb263cd56d9c486683a099df193a94
Harsh Sharma <krish9515274@gmail.com>
2026-09-02 15:36:52 +0530

    leet 287

---
+++ Arrays&Hashing/FindDuplicateNumber.java
@@ -0,0 +1,11 @@
+// leet 287
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean[] arr = new boolean[nums.length+1];
+        for(int i=0; i<nums.length; i++){
+            if(arr[nums[i]]) return nums[i];
+            else arr[nums[i]] = true;
+        }
+        return -1;
+    }
+}
