b38d14ddedb55111ad141d00a86d82fafae48eb7
Harsh Sharma <krish9515274@gmail.com>
2026-09-03 13:01:28 +0530

    leet 229

---
+++ Arrays&Hashing/MajorityElement2.java
@@ -0,0 +1,46 @@
+// leet 229
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0;
+        int maj1 = 0;
+        int count2 = 0;
+        int maj2 = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==maj1){
+                count1++;
+            }
+            else if(nums[i]==maj2){
+                count2++;
+            }
+            else if(count1==0){
+                maj1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2==0){
+                maj2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        int freq1 = 0;
+        int freq2 = 0;
+        for(int num:nums){
+            if(num==maj1) freq1++;
+            else if(num==maj2) freq2++;
+        }
+
+        if(freq1>nums.length/3){
+            result.add(maj1);
+        }
+        if(freq2>nums.length/3){
+            result.add(maj2);
+        }
+
+        return result;
+    }
+}
