1bbda3feee3edcffe388b3e1c098fd80b9668bc6
Harsh Sharma <krish9515274@gmail.com>
2026-09-07 19:24:50 +0530

    leet 160

---
+++ LinkedList/FindIntersection.java
@@ -0,0 +1,45 @@
+// leet 160
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getListLength(headA);
+        int lenB = getListLength(headB);
+
+        while(lenA>lenB){
+            lenA--;
+            headA = headA.next;
+        }
+
+        while(lenB>lenA){
+            lenB--;
+            headB = headB.next;
+        }
+
+        while(headA!=headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
+    private int getListLength(ListNode head){
+        ListNode temp = head;
+        int length = 0;
+        while(temp!=null){
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+}
