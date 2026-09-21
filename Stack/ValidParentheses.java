9045b34758210b3a5007badc0bb5ef0d1b9acd48
Harsh Sharma <krish9515274@gmail.com>
2026-09-21 17:29:35 +0000

    leet 20

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,19 @@
+// leet 20
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')' , '(');
+        map.put(']' , '[');
+        map.put('}' , '{');
+
+        for(char ch : s.toCharArray()){
+            if(map.containsKey(ch)){
+                if(!stack.isEmpty() && stack.peek()==map.get(ch)) stack.pop();
+                else return false;
+            }
+            else stack.push(ch);
+        }
+        return stack.isEmpty();
+    }
+}
